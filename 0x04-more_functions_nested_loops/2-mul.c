e46dfae2891f63cc717f54c9777a383c1d337480
tapilot <abovuschools@gmail.com>
2023-09-15 15:33:05 -0700

    2. Collaboration is multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * mul - Multiplies two integres
+ * mul - Multiplies two integers
  * @x - The first number to multiply
  * @y - The second number to multiply with first
  * Return: The result of multiplication between x param and y param
