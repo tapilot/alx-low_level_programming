f29d23bcc98d147a05e7944a5a5645cb77db8f6c
tapilot <abovuschools@gmail.com>
2023-09-18 06:32:33 -0700

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * reset_to_98 - Takes a pointer to an int and updates the value it points to 98
+ * reset_to_98 - Takes pointer to an int and updates the value it points to 98
  *
  * @n: An integer input pointer
  *
