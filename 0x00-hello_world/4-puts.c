d77d363ccfd46244ea9992d53a8453e7e861b3d6
tapilot <abovuschools@gmail.com>
2023-10-05 07:03:25 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,17 @@
 
 
 /**
- * main - prints "Programming is like building a multilingual
- * puzzle, followed by a newline."
- * Return 0.
+ * main - Entry point of the program.
+ *
+ * Description: This function prints "Programming is like building a multilingual
+ * puzzle" followed by a newline and returns 0.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 
 	return (0);
 }
