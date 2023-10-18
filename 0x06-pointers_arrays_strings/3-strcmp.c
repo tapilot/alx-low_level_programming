b6e10b630617cf27217974a1e948a34f22ed6e2f
tapilot <abovuschools@gmail.com>
2023-10-18 16:33:18 -0700

    Implement String Comparison Function

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,16 +9,12 @@
  * If str1 > str2, the positive difference of the first unmatched characters.
  *
  */
-
 int _strcmp(char *s1, char *s2)
-
 {
-
 	while (*s1 && *s2 && *s1 == *s2)
 	{
 	s1++;
 	s2++;
 	}
 	return (*s1 - *s2);
-
 }
