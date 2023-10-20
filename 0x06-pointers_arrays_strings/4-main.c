1b29d608413a1d92044ee6ca72d5f72269293d48
tapilot <abovuschools@gmail.com>
2023-10-20 08:02:28 -0700

    Add function to print integer

--- 0x06-pointers_arrays_strings/4-main.c
+++ 0x06-pointers_arrays_strings/4-main.c
@@ -2,48 +2,55 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- * @a: an array of integers
- * @n: the number of elements to swap
- *
- * Return: nothing.
+ * reverse_array - reverses an array of integers
+ * @a: the array to reverse
+ * @n: the number of elements in the array
  */
-
-void print_array(int *a, int n)
-
+void reverse_array(int *a, int n)
 {
-
+	int temp;
 	int i;
 
-	i = 0;
-	while (i < n)
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - 1 - i] = temp;
+		a[n - 1 - i] = temp;
+	}
+}
+
+/**
+ * print_array - prints an array of integers
+ * @a: the array to print
+ * @n: the number of elements to print
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
 	{
 		if (i != 0)
 		{
 			printf(", ");
 		}
 		printf("%d", a[i]);
-		i++;
 	}
 	printf("\n");
-
 }
 
 /**
- * main - check the code
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0
  */
-
 int main(void)
-
 {
-
 	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
 
 	print_array(a, sizeof(a) / sizeof(int));
 	reverse_array(a, sizeof(a) / sizeof(int));
 	print_array(a, sizeof(a) / sizeof(int));
-	return (0);
 
+	return (0);
 }
--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,21 +3,17 @@
 /**
  * reverse_array - Reverses the content of an array of integers.
  * @a: The array of integers to be reversed.
- * @n: the number of elements in the array.
+ * @n: The number of elements in the array.
  *
  */
-
-void reverse_array(int *a, int n)
-
+void reversed_array(int *a, int n)
 {
-
 	int tmp, index;
 
-	for(index = n - 1; index >= n / 2; index--)
+	for (index = n - 1; index >= n / 2; index--)
 	{
-	tmp = a[n - 1 - index];
-	a[n - 1 - index] = a[index];
-	a[index] = tmp;
+		tmp = a[n - 1 - index];
+		a[n - 1 - index] = a[index];
+		a[index] = tmp;
 	}
-
 }
