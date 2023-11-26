2d620ecd5573fbca83d441349d7be5c548202fe9
tapilot <abovuschools@gmail.com>
2023-11-26 06:17:02 -0800

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,13 +17,13 @@ unsigned int binary_to_uint(const char *b)
 	num = 0;
 	if (!b)
 		return (0);
-	
+
 	for (a = 0; b[a] != '\0'; a++)
 	{
 		if (b[a] != '0' && b[a] != '1')
 			return (0);
 	}
-	
+
 	for (a = 0; b[a] != '\0'; a++)
 	{
 		num <<= 1;
